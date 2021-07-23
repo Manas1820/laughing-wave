cc0d5ffd69fc90329f850a67ee4b61a4b07b1f2a
Manas1820 <manasgupta1820@gmail.com>
2021-07-24 01:43:16 +0530

    Added Wave Effect

--- main.js
+++ main.js
@@ -1,171 +1,225 @@
 import './style.css'
-// import * as THREE from 'three'
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
-//import gsap from 'gsap'
-//import * as dat from 'dat.gui'
+import * as dat from 'dat.gui'
+const fragmentShader = `
+precision highp float;
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
 
-//constant to define color of the mesh and add to debug
-const parameters = {
-  color:0xff0000
+varying float vElevation;
+
+void main()
+{
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+    
+    gl_FragColor = vec4(color, 1.0);
+}
+`
+const vertexShader = `
+precision highp float;
+varying float vElevation;
+
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+vec4 taylorInvSqrt(vec4 r)
+{
+    return 1.79284291400159 - 0.85373472095314 * r;
+}
+vec3 fade(vec3 t)
+{
+    return t*t*t*(t*(t*6.0-15.0)+10.0);
 }
 
+float cnoise(vec3 P)
+{
+    vec3 Pi0 = floor(P);
+    vec3 Pi1 = Pi0 + vec3(1.0);
+    Pi0 = mod(Pi0, 289.0);
+    Pi1 = mod(Pi1, 289.0);
+    vec3 Pf0 = fract(P);
+    vec3 Pf1 = Pf0 - vec3(1.0);
+    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+    vec4 iy = vec4(Pi0.yy, Pi1.yy);
+    vec4 iz0 = Pi0.zzzz;
+    vec4 iz1 = Pi1.zzzz;
+
+    vec4 ixy = permute(permute(ix) + iy);
+    vec4 ixy0 = permute(ixy + iz0);
+    vec4 ixy1 = permute(ixy + iz1);
+
+    vec4 gx0 = ixy0 / 7.0;
+    vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+    gx0 = fract(gx0);
+    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+    vec4 sz0 = step(gz0, vec4(0.0));
+    gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+    gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+    vec4 gx1 = ixy1 / 7.0;
+    vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+    gx1 = fract(gx1);
+    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+    vec4 sz1 = step(gz1, vec4(0.0));
+    gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+    gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+    g000 *= norm0.x;
+    g010 *= norm0.y;
+    g100 *= norm0.z;
+    g110 *= norm0.w;
+    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+    g001 *= norm1.x;
+    g011 *= norm1.y;
+    g101 *= norm1.z;
+    g111 *= norm1.w;
+
+    float n000 = dot(g000, Pf0);
+    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+    float n111 = dot(g111, Pf1);
+
+    vec3 fade_xyz = fade(Pf0);
+    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+    return 2.2 * n_xyz;
+}
+
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++)
+    {
+        elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }
+    
+    modelPosition.y += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    vElevation = elevation;
+}
+`
+
+
+
+const gui = new dat.GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.getElementById('webgl')
 
-
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Texture Loader
- */
+const waterGeometry = new THREE.PlaneBufferGeometry(2, 2, 512, 512)
+
+// Colors
+debugObject.depthColor = '#186691'
+debugObject.surfaceColor = '#9bd8ff'
+
+gui.addColor(debugObject, 'depthColor').onChange(() => { waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor) })
+gui.addColor(debugObject, 'surfaceColor').onChange(() => { waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
+
+// Material
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms:
+    {
+        uTime: { value: 0 },
+        
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+        uBigWavesSpeed: { value: 0.75 },
+
+        uSmallWavesElevation: { value: 0.15 },
+        uSmallWavesFrequency: { value: 3 },
+        uSmallWavesSpeed: { value: 0.2 },
+        uSmallIterations: { value: 4 },
+
+        uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+        uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+        uColorOffset: { value: 0.08 },
+        uColorMultiplier: { value: 5 }
+    }
+})
+
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
 
 
-const manager = new THREE.LoadingManager();
-manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallIterations, 'value').min(0).max(5).step(1).name('uSmallIterations')
 
-	console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
 
-};
-
-manager.onLoad =  ( ) => {
-	console.log( 'Loading complete!');
-};
-
-
-manager.onProgress = ( url, itemsLoaded, itemsTotal ) => {
-	console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-};
-
-manager.onError =  ( url ) => {
-	console.log( 'There was an error loading ' + url );
-};
-
-const textureLoader = new THREE.TextureLoader(manager);
-
-// Add a new texture like this const chessBoard = textureLoader.load('checkerboard-8x8.png')
-
-//const  = textureLoader.load('')
-
-
-
-/**
- * Axes Helper
- */
-
-// const axesHelper = new THREE.AxesHelper(2)
-// scene.add(axesHelper)
-
-/**
- * Objects
- */
-
-
-const material =  new THREE.MeshBasicMaterial({ color: parameters.color, wireframe: true })
-const mesh = new THREE.Mesh(
-  new THREE.BoxBufferGeometry(),
-  material
-)
- 
-scene.add(mesh)
-
-// const group = new THREE.Group()
-// group.scale.y = 2
-// group.rotation.y = 0.2
-// scene.add(group)
-
-// const cube1 = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-// )
-// cube1.position.x = - 1.5
-// group.add(cube1)
-
-// const cube2 = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-// )
-// cube2.position.x = 0
-// group.add(cube2)
-
-// const cube3 = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-// )
-// cube3.position.x = 1.5
-// group.add(cube3)
+// Mesh
+const water = new THREE.Mesh(waterGeometry, waterMaterial)
+water.rotation.x = - Math.PI * 0.5
+scene.add(water)
 
 /**
  * Sizes
  */
+
 const sizes = {
     width: innerWidth,
     height: innerHeight
 }
 
-/**
- * Debug
- */
 
-
-/*
-const gui = new dat.GUI()
-
-
-// to move coordinates
-gui.add(mesh.position, 'x', -3, 3, 0.01)
-gui.add(mesh.position, 'y', -3, 3, 0.01)
-gui.add(mesh.position, 'z', -3, 3, 0.01)
-
-// To add Checkbox
-gui.add(mesh, 'visible')
-gui.add(material, 'wireframe')
-
-
-//To add change of colors
-gui.addColor(parameters, 'color').onChange(() => {
-  material.color.set(parameters.color)
-})
-
-*/
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-camera.position.z = 3
-// camera.lookAt(new THREE.Vector3(0, - 1, 0))
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1,1,1)
 scene.add(camera)
 
-/**
- * Renderer
- */
+
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
   antialias:true,
 })
 renderer.setSize(sizes.width, sizes.height)
-
-
-// --------------------------------------------------------------------------------------
-
-//Mouse Movements
-const raycaster = new THREE.Raycaster()
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.enableZoom = false
-const mouse = {
-  x: undefined,
-  y: undefined
-}
-
-// -----------------------------------------------------------------------------------
-// Event Listners
-
-
 
 // To handle screen size changes 
 addEventListener('resize', () => {
@@ -181,63 +235,35 @@ addEventListener('resize', () => {
   renderer.setPixelRatio(devicePixelRatio)
 })
 
-// To handle full screen mode
 
-/*
-addEventListener('dblclick', () => {
-  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-  if (!fullscreenElement) {
-    if(canvas.requestFullscreen)
-      canvas.requestFullscreen()
-    else if (canvas.webkitFullscreenElement)
-      canvas.webkitFullscreen()
-  }
-  else {
-    if(document.exitFullscreen)
-      document.exitFullscreen()
-    else if (document.webkitExitFullscreen)
-      document.webkitExitFullscreen()
-  }
+/**
+ * Animate
+ */
 
-})
+const clock = new THREE.Clock()
 
-*/
+const raycaster = new THREE.Raycaster()
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
 addEventListener('mousemove', () => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1
   mouse.y = -(event.clientY / innerHeight) * 2 + 1
 })
 
-
-// ------------------------------------------------------------------------------------
-const clock = new THREE.Clock()
-
-//To add animations to the bock
-// gsap.to(cube2.position, { duration: 1, delay: 1, y: 1 })
-// gsap.to(cube2.position, { duration: 1, delay: 2, y: 0 })
-
-// Animations
-let time = Date.now()
-function animate() {
-  // Calculate the FPS
-  const currentTime = Date.now()
-  let deltaTime = currentTime - time
-  time = currentTime
-
-  //Another method is to use the Three js clock
-  const elaspedTime = clock.getElapsedTime()
-  
-  //To add move movement effects
-  // cube2.position.y = mouse.y * 0.07
-  // cube2.position.x = mouse.x * 0.07
-  
-  //cube3.position.y = Math.sin(elaspedTime)
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
-  raycaster.setFromCamera(mouse, camera)
-  controls.update()
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    waterMaterial.uniforms.uTime.value = elapsedTime
+    raycaster.setFromCamera(mouse, camera)
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
 }
 
+tick()
 
-
-animate()
